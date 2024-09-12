78df74f028c8b9be866650031f1aaba51a2943e2
Maximiliano Tabacman <mtabacman@gmail.com>
2024-09-11 22:53:47 -0300

    Trying to fix Gaming Table refresh

--- files/modules-1.0.0/gamingTable.js
+++ files/modules-1.0.0/gamingTable.js
@@ -1,12 +1,12 @@
 function configureServerSentEvents(eventSource) {
-    eventSource.addEventListener("dice-rolled", event=>{
-        jsonData = JSON.parse(event.data);
-        rollTo($("#" + jsonData.character + "-roll"), jsonData.roll);
-    }
-    );
     eventSource.addEventListener("player-character-hero-status-updated", event=>{
         jsonData = JSON.parse(event.data);
         $("#" + jsonData.character + '-status').click();
+    }
+    );
+	eventSource.addEventListener("dice-rolled", event=>{
+        jsonData = JSON.parse(event.data);
+        rollTo($("#" + jsonData.character + "-roll"), jsonData.roll);
     }
     );
 }
