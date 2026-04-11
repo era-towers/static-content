b09cc0ff19866d6575741ba0559140c887f70d4a
Maximiliano Tabacman <mtabacman@gmail.com>
2026-04-10 23:45:17 -0300

    Re-rolls

--- files/modules-1.0.0/adventuring.js
+++ files/modules-1.0.0/adventuring.js
@@ -2,9 +2,43 @@
 
 function configureServerSentEvents(eventSource) {
     eventSource.addEventListener("dice-rolled", event => {
-			$("#refresh-pending-declarations").click();
-    });
+        const jsonData = JSON.parse(event.data);
+        showToast(jsonData.character + " rolled " + jsonData.roll);
+        $("#refresh-pending-declarations").click();
+    }
+    );
     eventSource.addEventListener("declaration", event => {
-			$("#refresh-pending-declarations").click();
+        $("#refresh-pending-declarations").click();
+    }
+    );
+}
+
+function showToast(message) {
+    let $container = $('#ui-toast-container');
+    if ($container.length === 0) {
+        $container = $('<div>', {
+            id: 'ui-toast-container'
+        }).appendTo('body');
+    }
+
+    const $toast = $('<div>', {
+        class: 'ui-toast',
+        text: message
     });
+
+    $container.append($toast);
+
+    $toast.show('drop', {
+        direction: 'right'
+    }, 400);
+
+    setTimeout(function() {
+        $toast.hide('fade', {}, 400, function() {
+            $(this).remove();
+
+            if ($container.children().length === 0) {
+                $container.remove();
+            }
+        });
+    }, 3000);
 }
